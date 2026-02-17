0784bcc833844604338d4406a57debe3bed239fb
dhruv-epam <dhruvkamleshkumar_patel@epam.com>
2026-02-17 20:57:46 +0530

    test

--- app/main.py
+++ app/main.py
@@ -13,3 +13,4 @@ app.include_router(rooms.router)
 app.include_router(reservations.router)
 
 # This is a sample comment added to check the commit functionality.
+# THis is a bad bad code comment, but it is here to test the commit functionality. Please ignore this comment. It does not add any value to the codebase. It is just a placeholder comment to check if the commit functionality is working properly. This comment should be removed in the future as it does not provide any meaningful information about the code.
